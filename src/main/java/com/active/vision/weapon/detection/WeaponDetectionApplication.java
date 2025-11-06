e9b4846bb02d5289821dfc9a55ea3c06d2888fab
Siran Parwin <simranhaidar91@gmail.com>
2025-11-07 02:00:42 +0530

    Spring Initiallizer

---
+++ src/main/java/com/active/vision/weapon/detection/WeaponDetectionApplication.java
@@ -0,0 +1,13 @@
+package com.active.vision.weapon.detection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeaponDetectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeaponDetectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/active/vision/weapon/detection/WeaponDetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.active.vision.weapon.detection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeaponDetectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
